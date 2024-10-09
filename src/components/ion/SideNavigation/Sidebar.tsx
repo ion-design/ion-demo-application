120011452548355f2b6732a037b7af7a43489c93
Samraaj Bath <samraaj100@gmail.com>
2024-10-09 14:12:31 -0700

    Animate component: src/components/ion/SideNavigation/Sidebar.tsx

--- src/components/ion/SideNavigation/Sidebar.tsx
+++ src/components/ion/SideNavigation/Sidebar.tsx
@@ -1,5 +1,6 @@
 // ion/Sidebar: Generated with Ion on 2/4/2024, 7:46:45 PM
 import clsx from 'clsx';
+import { useEffect, useState } from 'react';
 
 interface Props {
 	className?: string;
@@ -8,12 +9,20 @@ interface Props {
 }
 
 export default function Sidebar({ children, filled = false, className }: Props) {
+	const [isMounted, setIsMounted] = useState(false);
+
+	useEffect(() => {
+		setIsMounted(true);
+	}, []);
+
 	return (
 		<div
 			className={clsx(
-				'flex h-screen min-h-0 flex-col justify-between border-r border-weak-stroke p-2',
+				'flex h-screen min-h-0 flex-col justify-between border-r border-weak-stroke p-2 transition-colors duration-300 ease-in-out',
 				{
 					'bg-primary-darker dark:bg-primary-lighter': filled,
+					'opacity-0 transform translate-x-[-20px]': !isMounted,
+					'opacity-100 transform translate-x-0': isMounted,
 				},
 				className
 			)}
